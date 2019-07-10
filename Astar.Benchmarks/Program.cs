5b73e357ddf5cf5147dce91d941ce7900c649c2c
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-07-10 14:53:05 +0200

    Add benchmarks

---
+++ Astar.Benchmarks/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BigSeed.Math;
+using BigSeed.Utils;
+
+namespace Astar.Benchmarks
+{
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]    
+    public class AstarBenchmark
+    {
+        DefaultMapInfo mapInfo;
+        PathFinding pathfinding;
+        int sizeX = 16;
+        int sizeY = 16;
+        int startX = 0;
+        int startY = 0;
+        int endX;
+        int endY;
+        
+        public AstarBenchmark()
+        {
+            endX = sizeX - 1;
+            endY = sizeY - 1;
+            
+            mapInfo = new DefaultMapInfo(sizeX, sizeY);
+            pathfinding = new PathFinding(mapInfo);
+        }
+        
+        string MapToString(int sizeX, int sizeY, int startX, int startY, int endX, int endY, List<int> cellIndexes, DefaultMapInfo pathMapLayer)
+        {
+            StringBuilder[] map = new StringBuilder[sizeY];
+
+            for (int i = 0; i < sizeY; i++)
+            {
+                map[i] = new StringBuilder(new String('_', sizeX));
+            }
+
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    if (!pathMapLayer.IsWalkable(x, y))
+                    {
+                        map[y][x] = 'o';
+                    }
+                }
+            }
+
+            foreach (var cellIndexe in cellIndexes)
+            {
+                Vector2Int position = MapUtils.IndexToCoords(cellIndexe, sizeX);
+
+                map[position.Y][position.X] = '#';
+            }
+
+            map[startY][startX] = 'S';
+            map[endY][endX] = 'E';
+
+            return String.Join("\n", map.Select(sb => sb.ToString()));
+        }
+
+        [Benchmark]
+        public List<int> SystemDiagnostic()
+        {
+            var path = pathfinding.FindPath(startX, startY, endX, endY);
+            
+//            string result = MapToString(sizeX, sizeY, startX, startY, endX, endY, path, mapInfo);
+//            Console.WriteLine(result);
+
+            return path;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<AstarBenchmark>();
+        }
+    }
+}
